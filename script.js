40e7f1a0d99709d6dc93b60bd84f4923459171cf
Morrese Green Jr <greenmo57@gmail.com>
2021-09-17 10:12:05 -0400

    First JavaScript Project

---
+++ script.js
@@ -0,0 +1,28 @@
+//This is a comment...
+
+
+console.log(typeof'This is and external JavaScript file!!!');
+
+console.log('Morrese');
+/**
+ * This is a funtction that logs text to the console
+ * 
+ * This is more commentary...
+ */
+
+console.log(12345)
+console.log(typeof 12345)
+console.log(typeof 123.456)
+console.log(typeof true)
+console.log(new Date)
+console.log(typeof null);
+console.log(typeof[1,2,3,4,5])
+const person =    {
+    "firstname":'Morrese',
+    "lastname":'Green Jr.',
+}
+
+function printName(){
+    console.log("Morrese Green Jr")
+}
+printName()
